fc22751cb551be35ca567df07bef7ea2d2be1abb
魏小伟 <1550518923@qq.com>
2020-12-01 16:53:34 +0800

    读写分离

---
+++ spring-shardingsphere/src/main/java/com/wxw/ShardingSphereMain.java
@@ -0,0 +1,17 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：wxw.
+ * @date ： 16:04 2020/12/1
+ * @description：ShardingSphere 启动类
+ * @version: v_0.0.1
+ */
+@SpringBootApplication
+public class ShardingSphereMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereMain.class,args);
+    }
+}
