d60769ae24c8f816e9986ad9cfc252840853c5d9
魏小伟 <815372184@qq.com>
2020-12-28 18:06:13 +0800

    memcached 整合和应用

---
+++ spring-zookeeper/src/main/java/com/wxw/leaderelection/LeaderElectionTest.java
@@ -0,0 +1,17 @@
+package com.wxw.leaderelection;
+
+import org.apache.zookeeper.server.quorum.LeaderElection;
+
+/**
+ * @author ：wxw.
+ * @date ： 15:32 2020/12/28
+ * @description：Leader 选举测试类
+ * @link:
+ * @version: v_0.0.1
+ */
+public class LeaderElectionTest {
+    public static void main(String[] args) {
+
+        // LeaderElection leaderElection = new LeaderElection("a");
+    }
+}
