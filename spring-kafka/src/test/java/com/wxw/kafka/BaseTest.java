1992a6e0d5ec3e6fae578e6c178896e65df42916
weixiaowei <weixiaowei@baijia.com>
2021-05-12 18:29:29 +0800

    Kafka stream test

--- spring-kafka/src/test/java/com/wxw/kafka/BaseTest.java
+++ spring-kafka/src/test/java/com/wxw/kafka/BaseTest.java
@@ -1,7 +1,11 @@
 package com.wxw.kafka;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
 
 /**
  * @author weixiaowei
@@ -15,4 +19,18 @@ public class BaseTest {
     public void test_path() {
         System.out.println("BaseTest.class.getResource(\"\") = " + BaseTest.class.getResource("/"));
     }
+
+    @Test
+    public void test_binary() throws IOException {
+        String filePath = "/Users/mac/IdeaProjects/wxw/wxw-springboot/spring-kafka/src/test/resources/高途课堂-report-0000000076000000";
+        BufferedInputStream inputStream = FileUtil.getInputStream(new File(filePath));
+        File file = new File("./test-" + System.currentTimeMillis() + ".json");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        OutputStream outputStream = new FileOutputStream(file);
+        IoUtil.copy(inputStream, outputStream);
+        outputStream.flush();
+    }
+
 }
--- spring-kafka/src/test/java/com/wxw/kafka/consumer/DemoAmpLogConsumer.java
+++ spring-kafka/src/test/java/com/wxw/kafka/consumer/DemoAmpLogConsumer.java
@@ -1,5 +1,7 @@
 package com.wxw.kafka.consumer;
 
+import cn.hutool.core.codec.Base64;
+import cn.hutool.core.util.ZipUtil;
 import com.alibaba.fastjson.JSON;
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.fasterxml.jackson.databind.ser.std.ByteArraySerializer;
@@ -10,6 +12,7 @@ import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -51,7 +54,7 @@ public class DemoAmpLogConsumer extends BaseTest {
         consumer.subscribe(Arrays.asList(topic));
         // Consumer调用poll方法来轮询Kafka集群的消息，一直等到Kafka集群中没有消息或者达到超时时间100ms为止
         while (true) {
-            ConsumerRecords<byte[], byte[]> records = consumer.poll(5);
+            ConsumerRecords<byte[], byte[]> records = consumer.poll(2);
             for (ConsumerRecord<byte[],byte[]> record : records) {
                 log.error("record topic = {}",record.topic());
                 log.error("record partition = {} offset = {}",record.partition(),record.offset());
@@ -59,14 +62,19 @@ public class DemoAmpLogConsumer extends BaseTest {
                 log.error("record value = {}",record.value());
                 //对record的value进行解析
                 // get a zip file instance
-                File file = new File("./test.json");
+                File file = new File("./test"+System.currentTimeMillis()+".json");
                 if (!file.exists()){
                     file.createNewFile();
                 }
                 OutputStream outputStream = new FileOutputStream(file);
-                byte[] uncompress = CompressHelper.uncompress(record.value());
-                outputStream.write(uncompress);
+                outputStream.write(record.value());
                 outputStream.flush();
+
+//                byte[] uncompress = CompressHelper.uncompress(record.value());
+//                byte[] uncompress = ZipUtil.unGzip(record.value());
+//                outputStream.write(uncompress);
+                outputStream.flush();
+//                outputStream.close();
             }
         }
     }
@@ -94,4 +102,56 @@ public class DemoAmpLogConsumer extends BaseTest {
         Future send = producer.send(producerRecord);
         log.error(JSON.toJSONString(send));
     }
+
+
+    @Test
+    public void test_path() {
+        System.out.println("BaseTest.class.getResource(\"\") = " + BaseTest.class.getResource("/"));
+    }
+
+    // 序列化字节数据
+    @Test
+    public void test_consumer_base64() throws IOException, DataFormatException {
+        String topic = "crashLog";
+        Properties props = new Properties();
+        // Kafka集群，多台服务器地址之间用逗号隔开
+        props.put("bootstrap.servers", "172.16.5.67:9092");
+        props.put("group.id", "test_group1");
+        // 自动提交offset到zk的时间间隔，时间单位是毫秒
+        props.put("auto.commit.interval.ms", "1000");
+        // 消息的反序列化类型
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        Consumer<String,String> consumer = new KafkaConsumer<>(props);
+        // 订阅的话题
+        consumer.subscribe(Arrays.asList(topic));
+        // Consumer调用poll方法来轮询Kafka集群的消息，一直等到Kafka集群中没有消息或者达到超时时间100ms为止
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(2);
+            for (ConsumerRecord<String, String> record : records) {
+                log.error("record topic = {}",record.topic());
+                log.error("record partition = {} offset = {}",record.partition(),record.offset());
+                log.error("record key = {}",record.key());
+                log.error("record value = {}",record.value());
+                //对record的value进行解析
+                // get a zip file instance
+                File file = new File("./test"+System.currentTimeMillis()+".json");
+                if (!file.exists()){
+                    file.createNewFile();
+                }
+                OutputStream outputStream = new FileOutputStream(file);
+//                byte[] decode = Base64.decode(record.value().getBytes(StandardCharsets.UTF_8));
+//                outputStream.write(decode);
+//                outputStream.flush();
+
+//                byte[] uncompress = CompressHelper.uncompress(decode);
+                byte[] uncompress = ZipUtil.unGzip(record.value().getBytes(StandardCharsets.UTF_8));
+                outputStream.write(uncompress);
+                outputStream.flush();
+//                outputStream.close();
+            }
+        }
+    }
+
+
 }
