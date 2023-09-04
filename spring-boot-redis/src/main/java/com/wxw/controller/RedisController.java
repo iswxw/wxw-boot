fb790b1a1ffafb74eb31a60287fbc12986fa6436
weixiaowei <1550518923@qq.com>
2023-09-05 00:24:05 +0800

    upd utils

---
+++ spring-boot-redis/src/main/java/com/wxw/controller/RedisController.java
@@ -0,0 +1,41 @@
+package com.wxw.controller;
+
+import com.wxw.common.utils.UtilsRedis;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @contract: 公众号：技术能量站
+ * @desc:
+ * @link:
+ */
+
+@Slf4j
+@RequestMapping("/redis")
+@RestController
+public class RedisController {
+
+    @Resource
+    private UtilsRedis redisUtil;
+
+    @RequestMapping("set")
+    public boolean redisset(String key, String value){
+        System.out.println(key+"--"+value);
+        return redisUtil.set(key,value);
+    }
+
+    @RequestMapping("get")
+    public Object redisget(String key){
+        System.out.println(redisUtil.get(key));
+        return redisUtil.get(key);
+    }
+
+    @RequestMapping("expire")
+    public boolean expire(String key,long ExpireTime){
+        return redisUtil.expire(key,ExpireTime);
+    }
+
+}
