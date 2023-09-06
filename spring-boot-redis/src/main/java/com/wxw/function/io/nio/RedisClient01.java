afa1c04ad879aa4565849f5e3af0494e290785f2
weixiaowei <1550518923@qq.com>
2023-09-06 09:52:39 +0800

    upd nio的原理

---
+++ spring-boot-redis/src/main/java/com/wxw/function/io/nio/RedisClient01.java
@@ -0,0 +1,33 @@
+package com.wxw.function.io.nio;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @contract: 公众号：技术能量站
+ * @desc:
+ * @link:
+ */
+public class RedisClient01 {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("------RedisClient01 start");
+
+        Socket socket = new Socket("127.0.0.1", 6379);
+        OutputStream outputStream = socket.getOutputStream();
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String string = scanner.next();
+            if (string.equalsIgnoreCase("quit")) {
+                break;
+            }
+            socket.getOutputStream().write(string.getBytes());
+            System.out.println("------input quit keyword to finish......");
+        }
+        outputStream.close();
+        socket.close();
+    }
+}
