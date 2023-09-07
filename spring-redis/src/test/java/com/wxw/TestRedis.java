619304fdc2f4718c0ebf3bbd6e14f2e66874b368
weixiaowei <1550518923@qq.com>
2023-09-07 23:45:39 +0800

    upd test redis env

--- spring-redis/src/test/java/com/wxw/TestRedis.java
+++ spring-redis/src/test/java/com/wxw/TestRedis.java
@@ -1,13 +1,28 @@
 package com.wxw;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
 /**
  * @contract: 公众号：技术能量站
  * @desc:
  * @link:
  */
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TestRedis {
 
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
 
-
+    @Test
+    public void TestHello(){
+        redisTemplate.opsForValue().set("k1","v1");
+    }
 
 }
